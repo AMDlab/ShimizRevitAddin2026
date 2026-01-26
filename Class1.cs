ee808bd99bdd7eba7dca714e4f08b385b4c52b26
Teki37 <jiahao.zhai@amd-lab.com>
2026-01-26 14:50:31 +0900

    Initial commit

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShimizRevitAddin2026
+{
+    public class Class1
+    {
+    }
+}
